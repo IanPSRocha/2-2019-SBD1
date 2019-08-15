f72d4ce6b4b04a71465d99afaaf30a46b93e0766
Ian Rocha <ianrocha.df@gmail.com>
2019-08-15 12:53:56 -0300

    Modelo final da resolução, falta apenas debugar

--- Exer01/exer01.c
+++ Exer01/exer01.c
@@ -4,9 +4,9 @@
 
 typedef struct cadastro{
     char nome[500];
-    int cpf;
-    int ano;
+    long int cpf;
     char carro[500];
+    int ano;
     char placa[8];
     struct cadastro *prox;
 }cadastro;
@@ -14,8 +14,7 @@ typedef struct cadastro{
 FILE *fp;
 char arquivo[] = "cadastro.txt";
 int n;
-int limite;
-cadastro *pessoa;
+cadastro *cabeca;
 
 void criaStruct();
 void abrirArquivo();
@@ -25,17 +24,18 @@ void acessaMenu();
 void exibirCarros();
 void adicionarCarros();
 void apagarCarros();
+void liberaStruct();
 
 int main(){
     criaStruct();
     abrirArquivo();
-    menu();
+    acessaMenu();
 return 0;
 }
 
 void criaStruct(){
-    pessoa = malloc(sizeof(cadastro));
-    pessoa->prox = NULL;
+    cabeca = malloc(sizeof(cadastro));
+    cabeca->prox = NULL;
 }
 
 void abrirArquivo(){
@@ -46,13 +46,19 @@ void abrirArquivo(){
     }
     else{
         while(!feof){
-            fscanf(fp, "%s", pessoa->nome);
-            fscanf(fp, "%d", pessoa->cpf);
-            fscanf(fp, "%d", pessoa->ano);
-            fscanf(fp, "%s", pessoa->carro);
-            fscanf(fp, "%s", pessoa->placa);
-
-
+            cadastro *novo;
+            novo = malloc(sizeof(cadastro));
+            if(novo == NULL){
+                printf("Erro ao alocar memoria(1)!\n");
+                exit(-1);
+            }
+            fscanf(fp, "%s", novo->nome);
+            fscanf(fp, "%ld", novo->cpf);
+            fscanf(fp, "%s", novo->carro);
+            fscanf(fp, "%d", novo->ano);
+            fscanf(fp, "%s", novo->placa);
+            novo->prox = cabeca->prox;
+            cabeca->prox = novo;
         }
     }
 }
@@ -67,9 +73,9 @@ void menu(){
     printf("=======================================================\n");
     printf("\t Por favor selecione a opção desejada:\n");
     printf(" (1) Exibir Carros cadastrados\n");
-    printf(" (2) Adicionar Carros cadastrados\n");
-    printf(" (3) Apagar Carro\n");
-    printf(" (4) Sair\n");
+    printf(" (2) Adicionar Carros no cadastro\n");
+    // printf(" (3) Apagar Carro\n");
+    printf(" (3) Sair\n");
     printf("=======================================================\n");
     printf("Digite a opção:");
 }
@@ -77,7 +83,7 @@ void menu(){
 void acessaMenu(){
     char opcao = '0';
     menu();
-    while(opcao != '4'){
+    while(opcao != '3'){
         scanf(" %c", &opcao);
         switch(opcao){
             case '1':
@@ -88,13 +94,14 @@ void acessaMenu(){
                 adicionarCarros();
                 menu();
                 break;
+            // case '3':
+            //     apagarCarros();
+            //     menu();
+            //     break;
             case '3':
-                apagarCarros();
-                menu();
-                break;
-            case '4':
                 system("clear");
                 fecharArquivo();
+                liberaStruct();
                 break;
             default:
                 printf("\nEntrada Inválida, digite novamente: ");
@@ -102,8 +109,66 @@ void acessaMenu(){
     }
 }
 
-void exibirCarros(){}
+void exibirCarros(){
+    cadastro *aux;
+    aux = cabeca;
+    for(aux; aux->prox != NULL; aux = aux->prox){
+        printf("Proprietário: %s\n", aux->nome);
+        printf("CPF: %ld\n", aux->cpf);
+        printf("Carro: %s\n", aux->carro);
+        printf("Ano: %d\n", aux->ano);
+        printf("Placa: %s\n", aux->placa);
+        printf("==============================\n");
+    }
+}
 
-void adicionarCarros(){}
+void adicionarCarros(){
+    cadastro *novocarro;
+    novocarro = malloc(sizeof(cadastro));
+    if(novocarro == NULL){
+        printf("Erro ao alocar memoria(2)!\n");
+        exit(-1);
+    }
+    printf("Digite o nome do propritário dono do veículo:\n");
+    scanf("%s", novocarro->nome);
+    printf("Digite o CPF do proprietário:\n");
+    scanf("%ld", &novocarro->cpf);
+    printf("Digite o modelo do carro:\n");
+    scanf("%s", novocarro->carro);
+    printf("Qual o ano do carro:\n");
+    scanf("%d", &novocarro->ano);
+    printf("Placa do veículo:\n");
+    scanf("%s", novocarro->placa);
 
-void apagarCarros(){}
+    fprintf(fp, "%s\n", novocarro->nome);
+    fprintf(fp, "%d\n", novocarro->cpf);
+    fprintf(fp, "%s\n", novocarro->carro);
+    fprintf(fp, "%d\n", novocarro->ano);
+    fprintf(fp, "%s\n", novocarro->placa);
+
+    printf("Cadastro Concluído!!\n");
+}
+
+// void apagarCarros(){
+//     char aux[500];
+//     cadastro *lixo;
+//     lixo = cabeca;
+//     printf("Digite o nome do carro que deseja apagar:\n");
+//     scanf("%s", aux);
+//     for(lixo; lixo->prox != NULL; lixo = lixo->prox){
+//         if(strcmp(aux, lixo->carro) == 0){
+
+//             free(lixo);
+//         }
+//     }
+// }
+
+void liberaStruct(){
+    cadastro *aux2, *prox1;
+    prox1 = cabeca;
+    while(prox1 != NULL){
+        aux2 = prox1;
+        prox1 = prox1->prox;
+        free(aux2);
+    }
+}
